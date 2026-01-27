bd9a4b53e58f08b64b61a7068174374bc0ac3560
FZRDHN <153108555+kakikakiseribu@users.noreply.github.com>
2026-01-27 17:47:07 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const weddingDate = new Date("2026-12-12");
+
+setInterval(() => {
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+
+    if (distance < 0) return;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    document.querySelector(".date").innerText = `${days} Hari menuju acara pernikahan`;
+}, 1000));
